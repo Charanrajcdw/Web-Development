81cf520808ac5d45b97f8f1d335c7dbadbedf42e
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-20 18:44:29 +0530

    document fragment added

--- Exercise-13/js/index.js
+++ Exercise-13/js/index.js
@@ -1,14 +1,35 @@
-import data from "../assets/friends.json" assert {type:'json'};
+import data from "../assets/friends.json" assert { type: "json" };
 
 const cardsContainer = document.getElementById("cardsContainer");
 
-let result = "";
+let result = new DocumentFragment();
 for (let friendDetail of data) {
-  result += '<div class="friend-card"><div class="friend-image">';
-  result += "<img src=" + friendDetail.img + " alt=" + friendDetail.first_name + " />";
-  result += '</div><div class="friend-details">';
-  result += '<p class="friend-name">' + friendDetail.first_name + " " + friendDetail.last_name + "</p>";
-  result += '<p class="friend-email">' + friendDetail.email + "</p>";
-  result += "</div></div>";
+  // image container
+  let friendImageContainer = document.createElement("div");
+  friendImageContainer.classList.add("friend-image");
+  let image = document.createElement("img");
+  image.setAttribute("src", friendDetail.img);
+  image.setAttribute("alt", friendDetail.first_name);
+  friendImageContainer.appendChild(image);
+
+  // detail container
+  let friendDetailsContainer = document.createElement("div");
+  friendDetailsContainer.classList.add("friend-details");
+  let friendName = document.createElement("p");
+  friendName.classList.add("friend-name");
+  friendName.innerText = friendDetail.first_name + " " + friendDetail.last_name;
+  let friendEmail = document.createElement("p");
+  friendEmail.classList.add("friend-email");
+  friendEmail.innerText = friendDetail.email;
+  friendDetailsContainer.appendChild(friendName);
+  friendDetailsContainer.appendChild(friendEmail);
+
+  // main container
+  let friendCardContainer = document.createElement("div");
+  friendCardContainer.classList.add("friend-card");
+  friendCardContainer.appendChild(friendImageContainer);
+  friendCardContainer.appendChild(friendDetailsContainer);
+  
+  result.appendChild(friendCardContainer);
 }
-cardsContainer.innerHTML = result;
+cardsContainer.appendChild(result);
