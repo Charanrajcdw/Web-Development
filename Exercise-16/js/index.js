032c1bf69f38bd08571a2813aaff36da31295d4e
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-23 15:52:03 +0530

    Modified review comments

--- Exercise-16/js/index.js
+++ Exercise-16/js/index.js
@@ -1,4 +1,4 @@
-const outputContainer = document.getElementById("outputContainer");
+const OUTPUT_CONTAINER = document.getElementById("outputContainer");
 
 /**
  * Bank closure function with all transaction functions
@@ -6,7 +6,7 @@ const outputContainer = document.getElementById("outputContainer");
  * @param {number} cardNumber - it is to find the user
  */
 function Bank(cardNumber) {
-  const userAccounts = [
+  const USER_ACCOUNTS = [
     { accountNumber: 1011, cardNumber: 1234, pin: 1234, balance: 1000 },
     { accountNumber: 1012, cardNumber: 2345, pin: 2345, balance: 2000 },
     { accountNumber: 1013, cardNumber: 3456, pin: 3456, balance: 3000 },
@@ -22,17 +22,16 @@ function Bank(cardNumber) {
    * @param {number} cardNumber - it is to validate and login the user
    */
   function login(cardNumber) {
-    const matchingAccount = userAccounts.filter((account) => account.cardNumber == cardNumber);
-    if (matchingAccount.length == 0) {
-      outputContainer.innerText = "INVALID CARD NO!!!";
+    currentUser = USER_ACCOUNTS.find((account) => account.cardNumber === cardNumber);
+    if (currentUser === undefined) {
+      OUTPUT_CONTAINER.innerText = "INVALID CARD NO!!!";
     } else {
-      currentUser = matchingAccount[0];
-      const pin = parseInt(prompt("Enter your pin:"));
-      if (currentUser.pin != pin) {
-        outputContainer.innerText = "INVALID PIN!!!";
+      const PIN = parseInt(prompt("Enter your pin:"));
+      if (currentUser.pin !== PIN) {
+        OUTPUT_CONTAINER.innerText = "INVALID PIN!!!";
       } else {
-        const transactionOption = parseInt(prompt("Enter 1.Withdraw 2.Deposit:"));
-        transaction(transactionOption);
+        const TRANSACTION_OPTION = parseInt(prompt("Enter 1.Withdraw 2.Deposit:"));
+        transaction(TRANSACTION_OPTION);
       }
     }
   }
@@ -53,7 +52,7 @@ function Bank(cardNumber) {
         deposit();
         break;
       default:
-        outputContainer.innerText = "INVALID TRANSACTION!!!";
+        OUTPUT_CONTAINER.innerText = "INVALID TRANSACTION!!!";
     }
   }
 
@@ -61,12 +60,12 @@ function Bank(cardNumber) {
    * This function withdraws amount from the user account
    */
   function withdraw() {
-    const amount = parseInt(prompt("Enter the amount to withdraw:"));
-    if (amount > currentUser.balance) {
-      outputContainer.innerText = "INVALID BALANCE!!!";
+    const AMOUNT = parseInt(prompt("Enter the amount to withdraw:"));
+    if (AMOUNT > currentUser.balance) {
+      OUTPUT_CONTAINER.innerText = "INVALID BALANCE!!!";
     } else {
-      currentUser.balance -= amount;
-      outputContainer.innerText = "Withdraw amount: " + amount + " Balance: " + currentUser.balance;
+      currentUser.balance -= AMOUNT;
+      OUTPUT_CONTAINER.innerText = `Withdraw amount: ${AMOUNT} Balance: ${currentUser.balance}`;
     }
   }
 
@@ -74,12 +73,12 @@ function Bank(cardNumber) {
    * This function deposits amount into the user account
    */
   function deposit() {
-    const amount = parseInt(prompt("Enter the amount to deposit:"));
-    currentUser.balance += amount;
-    outputContainer.innerText = "Deposit amount: " + amount + " Balance: " + currentUser.balance;
+    const AMOUNT = parseInt(prompt("Enter the amount to deposit:"));
+    currentUser.balance += AMOUNT;
+    OUTPUT_CONTAINER.innerText = `Deposit amount: ${AMOUNT} Balance: ${currentUser.balance}`;
   }
 }
 
-const cardNumber = parseInt(prompt("Enter your card number:"));
+const CARD_NUMBER = parseInt(prompt("Enter your card number:"));
 
-Bank(cardNumber);
+Bank(CARD_NUMBER);
