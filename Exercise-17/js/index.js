e35636fd88d2478d8d209efad826759ae2b93150
Charanrajcdw <charanraj.thiyagarajan@cdw.com>
2023-03-25 12:24:48 +0530

    Modified review comments

--- Exercise-17/js/index.js
+++ Exercise-17/js/index.js
@@ -11,37 +11,38 @@ $(function () {
 });
 
 /**
+ * This functions helps to create a element with given attributes and innertext
+ *
+ * @param {String} tag - the element to create
+ * @param {Object} attributes - the attributes of the element
+ * @param {String|Object} content - the innertext or innerhtml of the element
+ * @returns the created element
+ */
+function createCustomElement(tag, attributes, content) {
+  const element = $(tag);
+  for (let attributeName in attributes) {
+    element.attr(attributeName, attributes[attributeName]);
+  }
+  if (content) {
+    typeof content === String ? element.text(content) : element.append(content);
+  }
+  return element;
+}
+
+/**
  * This function dynamically adds the location details to the location tab
  */
 function addLocationDetails() {
-  const TABLE = $("<table>");
-  TABLE.addClass("location-table");
+  const TABLE = createCustomElement("<table>", { class: "location-table" });
   for (let location of locationData) {
-    //creating rows and columns
     const ROW = $("<tr>");
-    const IMAGE_COLUMN = $("<td>");
-    const STATE_COLUMN = $("<td>");
-    const CITY_COLUMN = $("<td>");
-    const LOCATION_COLUMN = $("<td>");
-
-    //adding image to row
-    const IMAGE = $("<img>");
-    IMAGE.attr("src", `assets/images/${location.country}.png`);
-    IMAGE_COLUMN.append(IMAGE);
-    ROW.append(IMAGE_COLUMN);
-
-    //adding location details to row
-    STATE_COLUMN.text(location.state);
-    ROW.append(STATE_COLUMN);
-    CITY_COLUMN.text(location.city);
-    ROW.append(CITY_COLUMN);
-    LOCATION_COLUMN.text(location.contact);
-    ROW.append(LOCATION_COLUMN);
-
-    //adding row to table
+    const IMAGE = createCustomElement("<img>", { src: `assets/images/${location.country}.png` });
+    ROW.append(createCustomElement("<td>", {}, IMAGE));
+    ROW.append(createCustomElement("<td>", {}, location.state));
+    ROW.append(createCustomElement("<td>", {}, location.city));
+    ROW.append(createCustomElement("<td>", {}, location.contact));
     TABLE.append(ROW);
   }
-
   $("#tabs-3").append(TABLE);
 }
 
